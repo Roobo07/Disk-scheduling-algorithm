5ba19e0e728a644a766ecf787efd7c6729b77df2
Roobo07 <164990082+Roobo07@users.noreply.github.com>
2024-09-23 10:36:39 +0530

    Update memory-allocation

--- memory-allocation.py
+++ memory-allocation.py
@@ -1,34 +1,32 @@
 #Memory Allocation Algorithm
-def ff(bsize, m, psize, n):
-alloc = [-1] * n
+def firstFit(blockSize,m,processSize,n):
+    allocation=[-1]*n
+   
+    for i in range(n):
+        for j in range(m):
+            if blockSize[j]>=processSize[i]:
+                allocation[i]=j
+                blockSize[j]-=processSize[i]
+                break
+    print("Process No. Process Size Block no.")
+    for i in range(n):
+        print("",i+1," ",processSize[i]," ",end="")
+        if allocation[i]!= -1:
+            print(allocation[i]+1)
+        else:
+            print("Not Allocated")
+           
+if __name__=='__main__':
+    m=int(input("Enter the number of memory blocks:"))
+    blockSize=[]
+    for i in range(m):
+        size=int(input(f"Enter size of memory block{i+1}:"))
+        blockSize.append(size)
 
-for i in range(n):
-for j in range(m):
-      		if bsize[j] >= psize[i]:
-        			alloc[i] = j
-        			bsize[j] -= psize[i]
-       			break
+    n=int(input("Enter the number of processes:"))
+    processSize=[]
+    for i in range(n):
+        size=int(input(f"Enter size of process{i+1}:"))
+        processSize.append(size)
 
-  	print("Process No. process Size Block no.")
-  	for i in range(n):
-    		print(" ", i+1, "   ", psize[i],"   ", end=" ")
-    		if alloc[i] ! = -1:
-      			print(alloc[i]+1)
-    		else:
-      			print("Not Allocated")
-
-if __name__ == "__main__":
-  	m = int(input("Enter the number of memory blocks:"))
-  	bsize = []
-  	for i in range(m):
-    		size = int(input(f"Enter size of memory block {i+1}: "))
-    		bsize.append(size)
-
-
-  	n = int(input("Enter the number of processes:"))
-  	psize = []
-  	for i in range(n):
-    		size = int(input(f"Enter size of process {i+1}:"))
-    		psize.append(size)
-
-  	ff(bsize, m, psize, n)
+    firstFit(blockSize,m,processSize,n) 
