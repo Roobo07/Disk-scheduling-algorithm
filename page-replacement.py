f3bf742f568eddb56eeeef4a0c51c27f27ac6f88
Roobo07 <164990082+Roobo07@users.noreply.github.com>
2024-09-23 10:24:48 +0530

    Update page-replacement

--- page-replacement.py
+++ page-replacement.py
@@ -1,37 +1,30 @@
 #page replacement algorithm
 def main():
-    n = int(input("Enter the number of pages: "))
-    a = list(map(int, input("Enter the page numbers: ").split()))
-    no = int(input("Enter the number of frames: "))
-    
-    frame = [-1] * no
-    j = 0  
-    count = 0  
+    n=int(input("Enter the number of pages:"))
+    a=list(map(int,input("Enter the page numbers: ").split()))
+    no=int(input("Enter the number of frames:"))
+    frame=[-1]*no
+    j=0
+    count=0
 
-    print("\t Ref string \t page frames")
+    print("\tRef String\tPage Frames")
+
     for i in range(n):
-        print(f"{a[i]}\t", end = " ")
-        avail = False
-
-     for k in range(no):
-if frame[k] == a[i]:
-avail = True
-break
-
-      if not avail:
-frame[j] = a[i]
-j = (j + 1) % no  
-count += 1
-
-
+        print(f"{a[i]}\t\t",end="")
+        avail=0
 
         for k in range(no):
-            	if frame[k] != -1:
-                		print(f"{frame[k]}\t", end = " ")
-            	else:
-                		print("-\t", end = " ")
-        print()  
-    print(f'Page fault: {count}')
+            if frame[k]==a[i]:
+                avail=1
+        if avail==0:
+            frame[j]=a[i]
+            j=(j+1)%no
+            count+=1
+            for k in range(no):
+                print(f"{frame[k]}\t",end="")
+        print()
+
+    print(f"Page Faults:{count}")
 
 if __name__ == "__main__":
-    main()
+    main() 
